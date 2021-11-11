03ebe280b1a0d08ccfa3f0a3b62271c12a5f0552
David Boyer <djb64@duke.edu>
2021-11-11 15:30:09 -0600

    removing unused code

--- lib/postgres_with/active_record.rb
+++ lib/postgres_with/active_record.rb
@@ -1,4 +1,3 @@
 require 'active_record'
 require 'postgres_with/active_record/relation'
-require 'postgres_with/active_record/cte_proxy'
 require 'postgres_with/active_record/querying'
--- lib/postgres_with/active_record/querying.rb
+++ lib/postgres_with/active_record/querying.rb
@@ -1,11 +1,3 @@
 module ActiveRecord::Querying
   delegate :with, to: :all
-
-  def from_cte(name, expression)
-    table = Arel::Table.new(name)
-
-    cte_proxy = CTEProxy.new(name, self)
-    relation = ActiveRecord::Relation.new cte_proxy, cte_proxy.arel_table
-    relation.with name => expression
-  end
 end
